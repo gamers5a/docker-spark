589b5a979f83a8d80b556dfc3b158913a4e21329
Axcel <gamers5a@mail.ru>
2018-10-18 11:53:41 +0300

    conda spark 2.3.1 as conda version on cluster

--- client/master.sh
+++ client/master.sh
@@ -16,6 +16,6 @@ export ANACONDA_ROOT=~/anaconda3
 export PYSPARK_DRIVER_PYTHON=$ANACONDA_ROOT/bin/ipython
 export PYSPARK_PYTHON=$ANACONDA_ROOT/bin/python
 
-/opt/conda/bin/conda install -c conda-forge pyspark findspark -y --quiet
+/opt/conda/bin/conda install -c conda-forge pyspark findspark -y
 
 /opt/conda/bin/conda install jupyter -y --quiet && mkdir /opt/notebooks && /opt/conda/bin/jupyter notebook --notebook-dir=/opt/notebooks --ip='*' --port=8888 --no-browser --allow-root
