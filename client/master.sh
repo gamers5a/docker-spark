79386c1396714507bd3096bff9cd7e54ec46973b
Axcel <gamers5a@mail.ru>
2018-10-18 10:47:03 +0300

    client mode

---
+++ client/master.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+export SPARK_MASTER_HOST=`hostname`
+
+. "/spark/sbin/spark-config.sh"
+
+. "/spark/bin/load-spark-env.sh"
+
+mkdir -p $SPARK_MASTER_LOG
+
+export SPARK_HOME=/spark
+
+ln -sf /dev/stdout $SPARK_MASTER_LOG/spark-master.out
+
+pip install jupyter && jupyter notebook
