d8b51b948f52582cf408bc34757dc7ba39d5ea04
Axcel <gamers5a@mail.ru>
2018-10-18 11:42:23 +0300

    pyspark install findspark

--- client/master.sh
+++ client/master.sh
@@ -12,5 +12,10 @@ export SPARK_HOME=/spark
 
 ln -sf /dev/stdout $SPARK_MASTER_LOG/spark-master.out
 
+export ANACONDA_ROOT=~/anaconda3
+export PYSPARK_DRIVER_PYTHON=$ANACONDA_ROOT/bin/ipython
+export PYSPARK_PYTHON=$ANACONDA_ROOT/bin/python
+
+/opt/conda/bin/conda install -c conda-forge pyspark findspark -y --quiet
 
 /opt/conda/bin/conda install jupyter -y --quiet && mkdir /opt/notebooks && /opt/conda/bin/jupyter notebook --notebook-dir=/opt/notebooks --ip='*' --port=8888 --no-browser --allow-root
