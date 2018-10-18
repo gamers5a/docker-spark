72ec5f28544a75d6c98e4fca67106a24a613d288
Axcel <gamers5a@mail.ru>
2018-10-18 21:00:27 +0300

    spark monitor extensions files

--- client/master.sh
+++ client/master.sh
@@ -14,4 +14,4 @@ ln -sf /dev/stdout $SPARK_MASTER_LOG/spark-master.out
 
 conda activate base
 
-mkdir /opt/notebooks && /opt/conda/bin/jupyter notebook --notebook-dir=/opt/notebooks --ip='*' --port=8888 --no-browser --allow-root
+mkdir /opt/notebooks && /opt/conda/bin/jupyter notebook --notebook-dir=/opt/notebooks --ip='*' --port=8888 --no-browser --allow-root --Spark.url="http://192.168.101.21:8080"
