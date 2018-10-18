da57201e66bedef61d5a8ac6a43164041ff91062
Axcel <gamers5a@mail.ru>
2018-10-18 12:45:00 +0300

    cloent base env

--- client/master.sh
+++ client/master.sh
@@ -12,5 +12,6 @@ export SPARK_HOME=/spark
 
 ln -sf /dev/stdout $SPARK_MASTER_LOG/spark-master.out
 
+conda activate base
 
 mkdir /opt/notebooks && /opt/conda/bin/jupyter notebook --notebook-dir=/opt/notebooks --ip='*' --port=8888 --no-browser --allow-root
