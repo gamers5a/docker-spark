8ffd730598a97e642fbb0f937569ecf4c9ccb484
Axcel <gamers5a@mail.ru>
2018-10-18 11:57:32 +0300

    update client docker build image

--- client/master.sh
+++ client/master.sh
@@ -12,10 +12,5 @@ export SPARK_HOME=/spark
 
 ln -sf /dev/stdout $SPARK_MASTER_LOG/spark-master.out
 
-export ANACONDA_ROOT=~/anaconda3
-export PYSPARK_DRIVER_PYTHON=$ANACONDA_ROOT/bin/ipython
-export PYSPARK_PYTHON=$ANACONDA_ROOT/bin/python
 
-/opt/conda/bin/conda install -c conda-forge pyspark findspark -y
-
-/opt/conda/bin/conda install jupyter -y --quiet && mkdir /opt/notebooks && /opt/conda/bin/jupyter notebook --notebook-dir=/opt/notebooks --ip='*' --port=8888 --no-browser --allow-root
+mkdir /opt/notebooks && /opt/conda/bin/jupyter notebook --notebook-dir=/opt/notebooks --ip='*' --port=8888 --no-browser --allow-root
